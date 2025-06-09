b8b3009c387d1f97ebb4f7a2d8747824a97cf72b
admin <bosko15082004@gmail.com>
2025-06-09 23:41:47 +0300

    feat: add a logic with monitors and 'synchronized'.

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,58 @@
-import java.util.concurrent.Semaphore;
-
 public class Main {
     public static final int MAX = 100;
 
     public static void main(String[] args) {
-        Semaphore oddSemaphore = new Semaphore(1);
-        Semaphore evenSemaphore = new Semaphore(0);
+        EvenOddPrinter printer = new EvenOddPrinter();
 
-        Runnable oddThread = new OddThread(oddSemaphore, evenSemaphore);
-        Runnable evenThread = new EvenThread(oddSemaphore, evenSemaphore);
+        Thread evenThread = new Thread(() -> {
+            for (int i = 0; i <= MAX; i += 2){
+                printer.printEven(i);
+            }
+        }, "EvenThread");
 
-        Thread realOddThread = new Thread(oddThread);
-        Thread realEvenThread = new Thread(evenThread);
+        Thread oddThread = new Thread(() -> {
+            for (int i = 1; i <= MAX; i += 2){
+                printer.printOdd(i);
+            }
+        }, "OddThread");
 
-        realOddThread.start();
-        realEvenThread.start();
+        evenThread.start();
+        oddThread.start();
 
         try {
-            realOddThread.join();
-            realEvenThread.join();
+            evenThread.join();
+            oddThread.join();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
 
-    static class OddThread implements Runnable{
-        private final Semaphore oddSemaphore;
-        private final Semaphore evenSemaphore;
+    static class EvenOddPrinter {
+        public boolean isEvenTurn = true;
 
-        OddThread(Semaphore oddSemaphore, Semaphore evenSemaphore) {
-            this.oddSemaphore = oddSemaphore;
-            this.evenSemaphore = evenSemaphore;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 1; i <= MAX; i += 2) {
-                try {
-                    oddSemaphore.acquire();
-                    System.out.println(i + " - printed by OddThread");
-                    evenSemaphore.release();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
+        public synchronized void printEven(int number) {
+            try {
+                while (!isEvenTurn) {
+                    wait();
                 }
+                System.out.println(number);
+                isEvenTurn = false;
+                notifyAll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
-    }
 
-    static class EvenThread implements Runnable{
-        private final Semaphore oddSemaphore;
-        private final Semaphore evenSemaphore;
-
-        EvenThread(Semaphore oddSemaphore, Semaphore evenSemaphore) {
-            this.oddSemaphore = oddSemaphore;
-            this.evenSemaphore = evenSemaphore;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 2; i <= MAX; i += 2) {
-                try {
-                    evenSemaphore.acquire();
-                    System.out.println(i + " - printed by EvenThread");
-                    oddSemaphore.release();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    return;
+        public synchronized void printOdd(int number) {
+            try {
+                while (isEvenTurn) {
+                    wait();
                 }
+                System.out.println(number);
+                isEvenTurn = true;
+                notifyAll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
